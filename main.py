ac55fce9d3ebeb67ed3036ef6a21c04f94d9a2e0
Abraão Állysson dos Santos Honório <abraao.allyson@eng.ci.ufpb.br>
2017-11-15 16:18:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,67 @@
-from flask import Flask, jsonify, request
-from bs4 import BeautifulSoup
-from urllib.request import urlopen, Request
-import os   
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+
 
 app = Flask(__name__)
+api = Api(app)
 
-#Criar GetByID: http://blog.luisrei.com/articles/flaskrest.html
+TODOS = {
+    
+}
 
-@app.route('/api/v1/filmes', methods=['GET'])
-def filmes():
-    html_doc = urlopen("http://www.adorocinema.com/filmes/numero-cinemas/").read()
-    soup = BeautifulSoup(html_doc, "html.parser")
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
 
-    data = []
-    for dataBox in soup.find_all("div", class_="card card-entity card-entity-list cf"):
-        nomeObj = dataBox.find("h2", class_="meta-title")
-        imgObj = dataBox.find(class_="thumbnail ")
-        sinopseObj = dataBox.find("div", class_="synopsis")
-        dataObj = dataBox.find(class_="meta-body").find(class_="meta-body-item meta-body-info")
+parser = reqparse.RequestParser()
+parser.add_argument('Código de barra')
+parser.add_argument('Nome')
+parser.add_argument('Preço')
+parser.add_argument('Ativo')
+parser.add_argument('Categoria'),
+parser.add_argument('Vencimento')
 
-        data.append({ 'nome': nomeObj.text.strip(),
-                        'poster' : imgObj.img['data-src'].strip(),
-                        'sinopse' : sinopseObj.text.strip(),
-                        'data' :  dataObj.text[1:23].strip().replace('/',' ')})
-                
-    return jsonify({'filmes': data})
+
+
+
+# Todo
+# shows a single todo item and lets you delete a todo item
+class Todo(Resource):
+    def get(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+    def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
+
+
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        id =   (len(TODOS) +1) #int(max(TODOS.keys()).lstrip('alimento')) 
+        todo_id = 'alimento%i' % id
+        TODOS[todo_id] = {'id': id,'Código de barra': args['Código de barra'],'Nome': args['Nome'],'Preço': args['Preço'],'Ativo': args['Ativo'],'Categoria': args['Categoria'],'Vencimento': args['Vencimento']}
+        return TODOS[todo_id], 201
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(TodoList, '/api/todos')
+api.add_resource(Todo, '/api//todos/<todo_id>')
+
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    # Tem que ser 0.0.0.0 para rodar no Heroku
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
