414992d8f68835a69ba7ef375415c8e1968d2899
Abraão Állysson dos Santos Honório <abraao.allyson@eng.ci.ufpb.br>
2017-11-15 16:37:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,22 @@
-from flask import Flask, jsonify, request,abort
-from bs4 import BeautifulSoup
-from urllib.request import urlopen, Request
-import os   
-
+from flask import Flask, url_for
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
+from flask import request
 
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
-
-@app.route('/api/v1/stock', methods=['GET'])
-def filmes():
-    html_doc = urlopen("http://globoesporte.globo.com/futebol/selecao-brasileira/").read()
-    soup = BeautifulSoup(html_doc, "html.parser")
-    a = ['Feijão', 'Arroz', 'Sabão', 'Carne', 'Biscoito']
-    b = [5.00, 3.50 , 2.55, 20.50, 1.7]
-    c = ['Alimentos', 'Alimentos', 'Limpeza', 'Carnes', 'Padaria']
-    at = [1,0,0,0,1]
-    l = list(range(10000000,10000005))
-    data = []
-    for i in range(5):
-       
-       data.append({'Código de barra' : l[i],
-                    'Nome:': a[i],
-                    'Preço:' : b[i],
-                    'Ativo:' : at[i],
-                    'Categoria' : c[i],
-                    'Vencimento' :  "07/11/2017"})
-                         #'Categoria' : c[i]})
-                        #'data' :  dataObj.text[1:23].strip().replace('/',' ')})
-                
-    return jsonify({'Estoque': data})
-
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
-
+@app.route('/echo', methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == 'GET':
+        return "ECHO: GET\n"
+    elif request.method == 'POST':
+        return "ECHO: POST\n"
+    elif request.method == 'PATCH':
+        return "ECHO: PACTH\n"
+    elif request.method == 'PUT':
+        return "ECHO: PUT\n"
+    elif request.method == 'DELETE':
+        return "ECHO: DELETE"
+    else:
+        return "WAAAA?"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run()
